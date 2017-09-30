2e6bca38f549b74ebd504a2d760ab5d63598c72a
ia <isaac.ardis@gmail.com>
2017-09-30 15:03:55 -0500

    delete dead commented code

--- Geotify/CatTilities.swift
+++ Geotify/CatTilities.swift
@@ -14,26 +14,6 @@ import CoreLocation
 import UIKit
 import CoreData
 
-// @objc(TrackPoint)
-// public class TrackPoint: NSManagedObject {
-//   @NSManaged public var name: String?
-//   @NSManaged public var lat: Float
-//   @NSManaged public var long: Float
-//   @NSManaged public var time: NSDate?
-//   @NSManaged public var heading: Float
-//   @NSManaged public var elevation: Float
-//   @NSManaged public var accuracy: Float
-//   @NSManaged public var speed: Float
-// }
-
-
-// extension TrackPoint {
-//   @nonobjc public class func fetchRequest() -> NSFetchRequest<TrackPoint> {
-//     return NSFetchRequest<TrackPoint>(entityName: "TrackPoint");
-//   }
-// }
-
-
 // send a TrackPoint model -> plain json dict
 func objectifyTrackpoint(trackpoint: TrackPoint) -> NSMutableDictionary? {
   let dict = NSMutableDictionary()
@@ -120,18 +100,6 @@ func clearTrackPointsCD() {
   print("Even deleting")
   amDeleting = true
   let moc = DataController().managedObjectContext
-  //let pointsFetch = NSFetchRequest<NSFetchRequestResult>(entityName: "TrackPoint")
-  //pointsFetch.includesPropertyValues = false //small feet
-  
-  // do {
-  //   let fetchedPoints = try moc.fetch(pointsFetch) as! [NSManagedObject]
-  //   for point in fetchedPoints {
-  //     moc.delete(point)
-  //   }
-  //   try moc.save() //mhmm
-  // } catch {
-  //   fatalError("Failed to fetch employees: \(error)")
-  // }
   
   // Create Fetch Request
   let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "TrackPoint")
@@ -195,88 +163,3 @@ func pushLocs() {
   }).resume()
   
 }
-
-
-
-
-//
-//
-//    // https://github.com/varshylmobile/LocationManager/blob/master/LocationManager.swift
-//    let arrayOfLocation = locations as NSArray
-//    let location = arrayOfLocation.lastObject as! CLLocation
-//    let coordLatLon = location.coordinate
-//
-//    let latitude  = coordLatLon.latitude
-//    let longitude = coordLatLon.longitude
-//
-//    // latitudeAsString  = coordLatLon.latitude.description
-//    // longitudeAsString = coordLatLon.longitude.description
-//
-//    // http://stackoverflow.com/questions/31937686/how-to-make-http-post-request-with-json-body-in-swift
-//    // json in body here
-//    // good old rayban https://www.raywenderlich.com/120442/swift-json-tutorial
-//    // and http://stackoverflow.com/questions/29512839/create-json-in-swift
-//    let json: [String: AnyObject] = [
-//      "name": "kitty" as AnyObject,
-//      "lat": latitude as AnyObject,
-//      "long": longitude as AnyObject
-//      //andmoar
-//
-//      // "altitude":
-//      // "transfer": [
-//      //   "startDate": "10/04/2015 12:45",
-//      //   "endDate": "10/04/2015 16:00"
-//      // ],
-//      // "custom": savedData
-//
-//    ]
-//
-//    let jsonData = try? JSONSerialization.data(withJSONObject: json)
-//
-//    // create post request
-//    let url = URL(string: "http://cattrack-155019.appspot.com/populate/")!
-//    var request = URLRequest(url: url)
-//    request.httpMethod = "POST"
-//    // http://stackoverflow.com/questions/39929513/http-post-request-with-json
-//    request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
-//
-//    // insert json data to the request
-//    request.httpBody = jsonData
-//
-//    _ = URLSession.shared.dataTask(with: request) { data, response, error in
-//        guard let data = data, error == nil else {
-//            print(error?.localizedDescription ?? "No data")
-//            return
-//        }
-//        let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
-//        if let responseJSON = responseJSON as? [String: Any] {
-//            print(responseJSON)
-//        }
-//    }
-
-
-// ------
-
-//but this has caps where others don't. don't trust it so much.
-// // http://stackoverflow.com/questions/24566180/how-to-post-a-json-with-new-apple-swift-language
-// // // create the request & response
-// var request = NSMutableURLRequest(URL: NSURL(string: "http://requestb.in/1ema2pl1"), cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData, timeoutInterval: 5)
-// var response: NSURLResponse?
-// var error: NSError?
-
-// // create some JSON data and configure the request
-// let jsonString = "json=[{\"str\":\"Hello\",\"num\":1},{\"str\":\"Goodbye\",\"num\":99}]"
-// request.HTTPBody = jsonString.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)
-// request.HTTPMethod = "POST"
-// request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-
-// // send the request
-// NSURLConnection.sendSynchronousRequest(request, returningResponse: &response, error: &error)
-
-// // look at the response
-// if let httpResponse = response as? NSHTTPURLResponse {
-//     println("HTTP response: \(httpResponse.statusCode)")
-// } else {
-//     println("No HTTP response")
-// }
-// //
