34be343ae5adda6e40fd7d6981693c38d9671235
jlanej <jlanej@gmail.com>
2018-10-21 17:28:49 -0500

    web based ipa install, maybe

---
+++ Geotify/catTracksDev/launch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rsync -avz ./getCats.html arew:/home/freyabison/punktlich.rotblauer.com/install/getCats.html
+rsync -avz ./cat.png arew:/home/freyabison/punktlich.rotblauer.com/install/cat.png
