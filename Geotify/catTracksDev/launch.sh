05b9e669dd9353921a46351d4f06a73ce0387107
jlanej <jlanej@gmail.com>
2018-10-21 19:59:29 -0500

    new dress for intstall

--- Geotify/catTracksDev/launch.sh
+++ Geotify/catTracksDev/launch.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 rsync -avz ./getCats.html arew:/home/freyabison/punktlich.rotblauer.com/install/getCats.html
-rsync -avz ./cat.png arew:/home/freyabison/punktlich.rotblauer.com/install/cat.png
+rsync -avz ./cat.png arew:/home/freyabison/punktlich.rotblauer.com/install/cat2.png
+
+
+rsync -avz ./install arew:/home/freyabison/punktlich.rotblauer.com/
