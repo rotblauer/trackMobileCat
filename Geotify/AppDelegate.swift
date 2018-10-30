f3de4baa96d9775578abc9388d92f56a357cf598
jlanej <jlanej@gmail.com>
2018-10-29 19:00:11 -0500

    clean MOC Delegation

--- Geotify/AppDelegate.swift
+++ Geotify/AppDelegate.swift
@@ -156,37 +156,4 @@ class DataController: NSObject {
     })
     return container
   }()
-  
-  // MARK: - Core Data Saving support
-//  var managedObjectContext: NSManagedObjectContext
-//
-//  override init() {
-//    // This resource is the same name as your xcdatamodeld contained in your project.
-//    guard let modelURL = Bundle.main.url(forResource: "TrackPoint", withExtension:"momd") else {
-//      fatalError("Error loading model from bundle")
-//    }
-//    // The managed object model for the application. It is a fatal error for the application not to be able to find and load its model.
-//    guard let mom = NSManagedObjectModel(contentsOf: modelURL) else {
-//      fatalError("Error initializing mom from: \(modelURL)")
-//    }
-//    let psc = NSPersistentStoreCoordinator(managedObjectModel: mom)
-//    self.managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
-//    self.managedObjectContext.persistentStoreCoordinator = psc
-//
-//    let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-//    let docURL = urls[urls.endIndex-1]
-//    /* The directory the application uses to store the Core Data store file.
-//     This code uses a file named "DataModel.sqlite" in the application's documents directory.
-//     */
-//    let storeURL = docURL.appendingPathComponent("TrackPoint.sqlite")
-//    do {
-//      try psc.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: nil)
-//    } catch {
-//      fatalError("Error migrating store: \(error)")
-//    }
-
-//  }
-  
-  
-  
 }
