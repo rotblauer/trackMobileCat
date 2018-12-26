566777339df8c407bc3c36c6bb17715f63cfe668
ia <isaac.ardis@gmail.com>
2018-12-25 21:23:43 -0600

    Fix orientation for catsnaps

--- Geotify/VCCatSnap.swift
+++ Geotify/VCCatSnap.swift
@@ -63,6 +63,7 @@ class CatSnapViewController: UIViewController, UIImagePickerControllerDelegate, 
     currentTripNotes.imgB64 = imgb64
     save(manager:locMan)
     pushLocs(force:true,pushToken: pushToken)
+    currentTripNotes.imgB64 = ""
     
     trackSnapButtonButton.isHidden = true
     saveSnapButton.isHidden = false
@@ -96,10 +97,76 @@ class CatSnapViewController: UIViewController, UIImagePickerControllerDelegate, 
   }
   func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
     let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
-    imagePicked.image = image
+    imagePicked.image = image.fixedOrientation()
     takeSnapButton.isHidden = true
     trackSnapButtonButton.isHidden = false
     resetSnapButton.isHidden = false
     dismiss(animated:true, completion: nil)
+  }
+}
+
+// https://gist.github.com/schickling/b5d86cb070130f80bb40#gistcomment-2393934
+extension UIImage {
+  
+  func fixedOrientation() -> UIImage? {
+    
+    guard imageOrientation != UIImage.Orientation.up else {
+      //This is default orientation, don't need to do anything
+      return self.copy() as? UIImage
+    }
+    
+    guard let cgImage = self.cgImage else {
+      //CGImage is not available
+      return nil
+    }
+    
+    guard let colorSpace = cgImage.colorSpace, let ctx = CGContext(data: nil, width: Int(size.width), height: Int(size.height), bitsPerComponent: cgImage.bitsPerComponent, bytesPerRow: 0, space: colorSpace, bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue) else {
+      return nil //Not able to create CGContext
+    }
+    
+    var transform: CGAffineTransform = CGAffineTransform.identity
+    
+    switch imageOrientation {
+    case .down, .downMirrored:
+      transform = transform.translatedBy(x: size.width, y: size.height)
+      transform = transform.rotated(by: CGFloat.pi)
+      break
+    case .left, .leftMirrored:
+      transform = transform.translatedBy(x: size.width, y: 0)
+      transform = transform.rotated(by: CGFloat.pi / 2.0)
+      break
+    case .right, .rightMirrored:
+      transform = transform.translatedBy(x: 0, y: size.height)
+      transform = transform.rotated(by: CGFloat.pi / -2.0)
+      break
+    case .up, .upMirrored:
+      break
+    }
+    
+    //Flip image one more time if needed to, this is to prevent flipped image
+    switch imageOrientation {
+    case .upMirrored, .downMirrored:
+      transform.translatedBy(x: size.width, y: 0)
+      transform.scaledBy(x: -1, y: 1)
+      break
+    case .leftMirrored, .rightMirrored:
+      transform.translatedBy(x: size.height, y: 0)
+      transform.scaledBy(x: -1, y: 1)
+    case .up, .down, .left, .right:
+      break
+    }
+    
+    ctx.concatenate(transform)
+    
+    switch imageOrientation {
+    case .left, .leftMirrored, .right, .rightMirrored:
+      ctx.draw(self.cgImage!, in: CGRect(x: 0, y: 0, width: size.height, height: size.width))
+    default:
+      ctx.draw(self.cgImage!, in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+      break
+    }
+    
+    guard let newCGImage = ctx.makeImage() else { return nil }
+    return UIImage.init(cgImage: newCGImage, scale: 1, orientation: .up)
   }
 }
